f95417a47d8e899d32ce835c074c689f70255b9b
wd <wdowny@mail.ru>
2018-12-27 09:23:57 +1000

    Java and env vars

---
+++ new/hadoop-env.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+export HADOOP_HOME=/home/hadoopuser/HA/hadoop
+export YARN_HOME=/home/hadoopuser/HA/hadoop
+export HBASE_HOME=/home/hadoopuser/HA/hbase
+export ZOOKEEPER_HOME=/home/hadoopuser/HA/zookeeper
+export HADOOP_MAPRED_HOME=/home/hadoopuser/HA/hadoop
+export JAVA_HOME=/usr/lib/jvm/java
+export PATH=${PATH}:${JAVA_HOME}/bin:/home/hadoopuser/HA/hadoop/bin:/home/hadoopuser/HA/hadoop/sbin:/home/hadoopuser/HA/hbase/bin:/home/hadoopuser/HA/zookeeper/bin:/home/hadoopuser/.local/bin:/home/hadoopuser/bin
+export HADOOP_HDFS_HOME=/home/hadoopuser/HA/hadoop
+export HADOOP_COMMON_HOME=/home/hadoopuser/HA/hadoop
+export HADOOP_CONF_DIR=/home/hadoopuser/HA/hadoop/etc/hadoop
+export YARN_CONF_DIR=/home/hadoopuser/HA/hadoop/etc/hadoop
