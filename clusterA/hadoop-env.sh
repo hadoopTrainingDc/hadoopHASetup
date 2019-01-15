6177d98d0b26961b12cf1d5c77679fe3c7f857bd
wd <wdowny@mail.ru>
2019-01-15 12:43:30 +1000

    another cluster deployment

---
+++ clusterA/hadoop-env.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+export HADOOP_HOME=/home/hadoopuser/HA/hadoop
+export YARN_HOME=/home/hadoopuser/HA/hadoop
+export HBASE_HOME=/home/hadoopuser/HA/hbase
+export ZOOKEEPER_HOME=/home/hadoopuser/HA/zookeeper
+export HADOOP_MAPRED_HOME=/home/hadoopuser/HA/hadoop
+export HIVE_HOME=/home/hadoopuser/HA/hive
+export JAVA_HOME=/usr/lib/jvm/java
+export PATH=${PATH}:${JAVA_HOME}/bin:/home/hadoopuser/HA/hadoop/bin:/home/hadoopuser/HA/hadoop/sbin:/home/hadoopuser/HA/hbase/bin:/home/hadoopuser/HA/zookeeper/bin:/home/hadoopuser/HA/hive/bin:/home/hadoopuser/.local/bin:/home/hadoopuser/bin
+export HADOOP_HDFS_HOME=/home/hadoopuser/HA/hadoop
+export HADOOP_COMMON_HOME=/home/hadoopuser/HA/hadoop
+export HADOOP_CONF_DIR=/home/hadoopuser/HA/hadoop/etc/hadoop
+export YARN_CONF_DIR=/home/hadoopuser/HA/hadoop/etc/hadoop
